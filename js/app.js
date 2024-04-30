c4d9bd0946e0cb649b5465f9137731be3836f5da
NaZz-x86 <nicolas.laurent2024@campus-eni.fr>
2024-04-30 14:04:34 +0200

    Save de mi-journée, beaucoup de modification

---
+++ js/app.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Sélection du formulaire
+    const form = document.getElementById("Validation");
+
+    // Écouteur d'événement pour la soumission du formulaire
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); // Empêche l'envoi du formulaire
+
+        // Sélection des champs de formulaire
+        const userName = document.getElementById("name2").value;
+        const userEmail = document.getElementById("mail2").value;
+        const password = document.getElementById("motDePasse").value;
+        const confirmPassword = document.getElementById("confMotDePasse").value;
+        //Caractére accepter:
+        const accepterNom = /^[a-zA-Z\-]+$/;
+        const accepterMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const accepterMdp = /^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-`~{\[}\]:;'\|,./<>?])[A-Za-z0-9!@#$%^&*()_+=-`~{\[}\]:;'\|,./<>?]{12,}$/;
+        if (accepterNom.test(userName)){
+        localStorage.setItem("user_name", userName);
+        const balise = document.getElementById("erreur-name");
+        const message = "Votre nom est correcte"; 
+        balise.style.color = "green"
+        balise.textContent = message
+       }else{
+        const balise = document.getElementById("erreur-name");
+        const message = "Votre nom est incorrecte"; 
+        balise.style.color = "red"
+        balise.textContent = message
+       }
+        if (accepterMail.test(userEmail)){
+        localStorage.setItem("user_mail", userEmail);
+        const balise = document.getElementById("erreur-mail");
+        const message = "Votre mail est correcte"; 
+        balise.style.color = "green"
+        balise.textContent = message
+       }else{
+        const balise = document.getElementById("erreur-mail");
+        const message = "Votre mail est incorrecte"; 
+        balise.style.color = "red"
+        balise.textContent = message
+       }
+        if (accepterMdp.test(password)) { 
+          if(password === confirmPassword) {
+          localStorage.setItem("password", password);
+          const balise = document.getElementById("erreur-motdepasse");
+          const message = "Votre mot de passe est correcte"; 
+          balise.style.color = "green";
+          balise.textContent = message;
+          }else(password !== confirmPassword) { 
+          const message ="Votre mot de passe ne sont pas identique";
+          const balise = document.getElementById("pasLeMemeMotDePasse")
+          balise.style.color = "Red";
+          balise.textContent = message;
+      }}else{
+        const balise = document.getElementById("erreur-motdepasse");
+        const message = "Le mot de passe n'est pas correcte"; 
+        balise.style.color = "red"
+        balise.textContent = message
+       }
+        // Stockage des données dans le localStorage
+        
+    
+
+        // Réinitialisation des champs de formulaire après la soumission
+        form.reset();
+
+        // Confirmation pour l'utilisateur
+        alert("Les données ont été enregistrées avec succès dans le localStorage !");
+    });
+});
